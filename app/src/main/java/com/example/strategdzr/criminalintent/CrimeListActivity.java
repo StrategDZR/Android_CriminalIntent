d7a0b850fe1397f89ca84532a42e4b0baa7b7d5c
Alexey Shein <strategdzr@gmail.com>
2017-03-31 11:37:36 +0300

    add fragments core

--- app/src/main/java/com/example/strategdzr/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/strategdzr/criminalintent/CrimeListActivity.java
@@ -2,10 +2,14 @@ package com.example.strategdzr.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by user on 31/03/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity{
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/com/example/strategdzr/criminalintent/CrimeListFragment.java
@@ -0,0 +1,13 @@
+package com.example.strategdzr.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 31/03/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+    //pass
+
+}
